65778af4e22a87a1ed5c457ef334e7bb2b6eb522
Yaniel <yanielsanchez930316@gmail.com>
2024-09-29 20:06:18 -0300

    CREATE: creating routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+import { TaskManagerComponent } from './components/task-manager/task-manager.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Pantalla inicial con botones
+  { path: 'create-task', component: TaskManagerComponent }, // Ruta para crear tareas
+  { path: 'list-tasks', component: TaskListComponent }, // Ruta para listar tareas
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
